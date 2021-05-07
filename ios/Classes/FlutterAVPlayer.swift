095d64a719b8be88ba7d2cdabf137600672f3482
brackendev <bracken.spencer@gmail.com>
2021-05-07 09:25:49 +0200

    Update FlutterAVPlayer.swift

--- ios/Classes/FlutterAVPlayer.swift
+++ ios/Classes/FlutterAVPlayer.swift
@@ -12,7 +12,7 @@ import Flutter
 
 class FlutterAVPlayer: NSObject, FlutterPlatformView {
     private var _flutterAVPlayerViewController : AVPlayerViewController;
-    
+
     init(frame:CGRect,
           viewIdentifier: CLongLong,
           arguments: Dictionary<String, Any>,
@@ -22,20 +22,18 @@ class FlutterAVPlayer: NSObject, FlutterPlatformView {
         if let urlString = arguments["url"] {
             let item = AVPlayerItem(url: URL(string: urlString as! String)!)
             _flutterAVPlayerViewController.player = AVPlayer(playerItem: item)
-        } else if let filePath = arguments["file"] {
-            let appDelegate = UIApplication.shared.delegate as! FlutterAppDelegate
-            let vc = appDelegate.window.rootViewController as! FlutterViewController
-            let lookUpKey = vc.lookupKey(forAsset: filePath as! String)
-            if let path = Bundle.main.path(forResource: lookUpKey, ofType: nil) {
-                let item = AVPlayerItem(url: URL(fileURLWithPath: path))
-                _flutterAVPlayerViewController.player = AVPlayer(playerItem: item)
-            }
+        } else if let fileName = arguments["file"] {
+            let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+            let documentsDirectory = paths[0]
+            let downloadsDirectory = documentsDirectory.appendingPathComponent("Downloads")
+            let url = downloadsDirectory.appendingPathComponent(fileName)
+            let item = AVPlayerItem(url: url)
+            _flutterAVPlayerViewController.player = AVPlayer(playerItem: item)
         }
         _flutterAVPlayerViewController.player!.play()
     }
     func view() -> UIView {
         return _flutterAVPlayerViewController.view;
     }
-    
-}
 
+}
